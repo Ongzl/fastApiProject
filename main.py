34d6d4a3d876f8fd9302156fd523501e6397ac10
Ong Zheng Lin <ongzhenglin1999@gmail.com>
2024-01-24 11:31:09 +0800

    added validation for add/del, increase data stored and added unit test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from random import randint
 from fastapi.encoders import jsonable_encoder
 import logging
+from typing import Union
 
 logging.basicConfig(filename="api.log",
                     format='%(asctime)s %(message)s',
@@ -12,7 +13,9 @@ logging.basicConfig(filename="api.log",
 logger = logging.getLogger()
 
 class Person(BaseModel):
+    id: Union[str,None] = None
     name: str
+    number: str
 
 def isNameInDict(name, dict):
     for val in dict.values():
@@ -34,6 +37,7 @@ async def hello():
 async def get(id: str):
     with open('data.json', 'r') as read_file:
         data = json.load(read_file)
+        #check that user not trying to get extra info(number of person in db), and that id exists
         if id in data and not id == 'max':
             logger.info(f'get person id: {id}')
             return data[id]
@@ -48,9 +52,15 @@ async def add(person: Person):
         file_data = json.load(file)
         print(person.name)
         print(file_data.values())
+        #name and number validation
+        if not (person.name.isalpha() and person.number.isnumeric()):
+            return "invalid name or phone number. only alphabets in name and numbers in number"
+        #check for repeated name
         if not isNameInDict(person.name, file_data):
             ind = file_data["max"] + 1
             file_data["max"] = file_data["max"] + 1
+            #set optional id of person to be same as dict index
+            person.id = ind
             file_data[ind] = jsonable_encoder(person)
             file.seek(0)
             json.dump(file_data, file)
@@ -61,13 +71,18 @@ async def add(person: Person):
             return "person already exist"
 
 @app.post("/del")
-async def delete(id:str):
+async def delete(id:str, name:str):
     with open('data.json', 'r+') as file:
         file_data = json.load(file)
         deleted = "NULL"
         if id in file_data:
-            deleted = file_data.pop(id)
-            logger.info(f"deleted person {id}: {deleted['name']}")
+            #check that name supplied is same as name in db
+            if file_data[id]['name'] == name:
+                deleted = file_data.pop(id)
+                logger.info(f"deleted person {id}: {deleted['name']}")
+            else:
+                logger.warning(f'attempt to delete id: {id}, but name mismatch')
+                return "name and id mismatch"
         else:
             logger.warning(f'attempt to delete invalid id: {id}')
         file.seek(0)
